e1642d5d738fbbd30fd187e985ecd8fec547b7fe
ryoken nakayama <ryoken7090@gmail.com>
2021-06-14 21:55:00 +0900

    scaffoldで土台を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :memos
   get 'hello' => 'posts#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
