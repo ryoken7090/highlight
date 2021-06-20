62ee0c5504efc14b462aee75f202c74d92f18ef5
ryoken nakayama <ryoken7090@gmail.com>
2021-06-20 14:47:02 +0900

    scaffoldコマンド実行

---
+++ spec/views/diaries/show.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "diaries/show", type: :view do
+  before(:each) do
+    @diary = assign(:diary, Diary.create!(
+      high_light: "MyText"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/MyText/)
+  end
+end
