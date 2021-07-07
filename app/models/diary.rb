a351f0b6ff93194a0910819b1ecf5c1f41cab8af
Daisuke Tsuji <dim0627@gmail.com>
2021-07-07 14:40:17 +0900

    モデルのバリデーションを追加

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -2,4 +2,11 @@
 
 class Diary < ApplicationRecord
   validates :high_light, presence: true
+  validates :concentration, presence: true
+  validates :concentration, inclusion: { in: 1..10 }
+  validates :energy, presence: true
+  validates :energy, inclusion: { in: 1..10 }
+  validates :reflection, presence: true
+  validates :thanks, presence: true
+  validates :date, presence: true
 end
