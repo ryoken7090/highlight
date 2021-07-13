497de50571e2f4fe865fa95fe98470554a0f1b95
Daisuke Tsuji <dim0627@gmail.com>
2021-07-13 14:41:33 +0900

    不要なValidationを削除

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -6,7 +6,5 @@ class Diary < ApplicationRecord
   validates :concentration, inclusion: { in: 1..10 }
   validates :energy, presence: true
   validates :energy, inclusion: { in: 1..10 }
-  validates :reflection, exclusion: { in: [nil] }
-  validates :thanks, exclusion: { in: [nil] }
   validates :date, presence: true
 end
