926e5f66491781086161aaaaf4b1262b4b5ecedc
ryoken nakayama <ryoken7090@gmail.com>
2021-07-14 08:59:53 +0900

    concentrationに対するテストを作成

--- spec/models/diary_spec.rb
+++ spec/models/diary_spec.rb
@@ -3,26 +3,67 @@
 require 'rails_helper'
 
 RSpec.describe Diary, type: :model do
-  describe "Validation" do
-    describe ".high_light" do
+  describe 'Validation' do
+    describe '.high_light' do
       let(:diary) { build :diary, high_light: high_light }
 
       subject { diary }
 
-      context "nil" do
+      context 'nil' do
         let(:high_light) { nil }
 
         it { is_expected.to be_invalid }
       end
 
-      context "空文字" do
-        let(:high_light) { "" }
+      context '空文字' do
+        let(:high_light) { '' }
 
         it { is_expected.to be_invalid }
       end
 
-      context "文字列" do
-        let(:high_light) { "text" }
+      context '文字列' do
+        let(:high_light) { 'text' }
+
+        it { is_expected.to be_valid }
+      end
+    end
+    describe '.concentration' do
+      let(:diary) { build :diary, concentration: concentration }
+
+      subject { diary }
+
+      context 'nil' do
+        let(:concentration) { nil }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context 'less than 1' do
+        let(:concentration) { 0 }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context 'bigger than 10' do
+        let(:concentration) { 11 }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context 'less than 1' do
+        let(:concentration) { 0 }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context '文字列' do
+        let(:concentration) { 'よん' }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context '1~10' do
+        let(:concentration) { (1..10).to_a.sample }
 
         it { is_expected.to be_valid }
       end
