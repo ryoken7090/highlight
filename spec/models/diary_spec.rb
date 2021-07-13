3f4fb063dd6912fa7a33384f55b0444a775009fa
Daisuke Tsuji <dim0627@gmail.com>
2021-07-13 14:49:02 +0900

    high_lightに対するテストを作成

--- spec/models/diary_spec.rb
+++ spec/models/diary_spec.rb
@@ -3,5 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe Diary, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validation" do
+    describe ".high_light" do
+      let(:diary) { build :diary, high_light: high_light }
+
+      subject { diary }
+
+      context "nil" do
+        let(:high_light) { nil }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context "空文字" do
+        let(:high_light) { "" }
+
+        it { is_expected.to be_invalid }
+      end
+
+      context "文字列" do
+        let(:high_light) { "text" }
+
+        it { is_expected.to be_valid }
+      end
+    end
+  end
 end
